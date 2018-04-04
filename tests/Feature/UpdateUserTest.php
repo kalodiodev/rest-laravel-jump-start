e2bb0eb59a7b7e7f497b7e388e46255023fdfb94
Athanasios Raptodimos <thanosra@gmail.com>
2018-04-04 07:23:18 +0300

    User update validation tests

--- tests/Feature/UpdateUserTest.php
+++ tests/Feature/UpdateUserTest.php
@@ -4,6 +4,7 @@ namespace Tests\Feature;
 
 use App\User;
 use Tests\TestCase;
+use Laravel\Passport\Passport;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
@@ -11,24 +12,56 @@ class UpdateUserTest extends TestCase
 {
     use RefreshDatabase;
 
+    private $user;
+
+    private $data = [
+        'name' => 'New name',
+        'email' => 'newmail@example.com',
+        'password' => 'New password',
+        'password_confirmation' => 'New password',
+    ];
+
+    public function setUp()
+    {
+        parent::setUp();
+
+        $this->user = factory(User::class)->create();
+    }
+
     /** @test */
     public function a_user_with_token_can_perform_a_profile_update()
     {
-        $user = factory(User::class)->create();
+        Passport::actingAs($this->user);
 
-        \Laravel\Passport\Passport::actingAs($user);
-
-        $response = $this->patchJson(route('api.update'), [
-            'name' => 'New name',
-            'email' => 'newmail@example.com',
-            'password' => 'New password',
-            'password_confirmation' => 'New password',
-        ])->assertStatus(200);
+        $response = $this->patchJson(route('api.update'), $this->data)
+            ->assertStatus(200);
 
         $this->assertDatabaseHas('users', [
-            'id' => $user->id,
-            'name' => 'New name',
-            'email' => 'newmail@example.com'
+            'id' => $this->user->id,
+            'name' => $this->data['name'],
+            'email' => $this->data['email']
         ]);
+    }
+
+    /** @test */
+    public function a_user_cannot_be_updated_with_invalid_data()
+    {
+        Passport::actingAs($this->user);
+
+        $response = $this->patchJson(route('api.update'), [
+            'email' => 'test',
+            'password' => '123',
+            'password_confirmation' => '123'
+        ])->assertStatus(422)->json('errors');
+
+        $this->assertArrayHasKey('email', $response);
+        $this->assertArrayHasKey('password', $response);
+    }
+
+    /** @test */
+    public function an_unauthenticated_user_cannot_be_updated()
+    {
+        $response = $this->patchJson(route('api.update'), $this->data)
+            ->assertStatus(401);
     }
 }
